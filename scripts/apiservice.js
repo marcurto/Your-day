f7543e6dfa7c97f95ad6cb35502e225ad72d6f80
marcurto <mcurtogilabert@gmail.com>
2020-10-14 00:23:40 +0200

    imdb connected

--- scripts/apiservice.js
+++ scripts/apiservice.js
@@ -28,6 +28,15 @@ async function getFictionBooks() {
 }
 getFictionBooks();
 
+async function getnoFictionBooks() {
+    // Get No Fiction Books
+    const response = await fetch(baseURL + "non-fiction-books");
+    const nofictionbooks = await response.json();
+    return nofictionbooks;
+
+}
+getnoFictionBooks();
+
 
 
 async function getGeniusElements(title, artist){
@@ -49,6 +58,33 @@ async function getGeniusElements(title, artist){
     return responsejson
 }
 
+async function getIMDBElements(title){
+    const idSearch = {
+        s: title
+    };
+    const pelis = await fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&"+ new URLSearchParams(idSearch), {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+            "x-rapidapi-key": "090d0c4b70msh0adca47ea867b16p1ea1efjsnf14c5d664816"
+        }
+    })
+    const data =  await pelis.json();
+    const id = data.Search[0].imdbID;
+    const moviesSearch = {
+        i: id
+    }
+    const response = await fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?r=json&" + new URLSearchParams(moviesSearch), {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+		"x-rapidapi-key": "090d0c4b70msh0adca47ea867b16p1ea1efjsnf14c5d664816"
+	}
+})
+
+    return await response.json();
+}
+
 
 
 
@@ -63,7 +99,9 @@ function generateHTMLsongs(data, date) {
     })
     document.getElementById('song-name').innerHTML = songDate[0].songTitle;
     document.getElementById('album-name').innerHTML = 'Cantante: ' + songDate[0].artist;
+    document.getElementById('days-number-one').innerHTML = 'Desde el día ' + songDate[0].startDate + ' hasta el día ' + songDate[0].endDate
     
+
     return songDate[0]; // Li apliquem return perquè és una funció síncrona de la qual en necessitem el resultat per cridar-lo a la funció asíncrona getGeniusElements.
 }
 
@@ -79,6 +117,7 @@ function generateHTMLmovies(data, date) {
     document.getElementById('movie-name').innerHTML = movieDate[0].movieTitle;
 
     console.log(movieDate)
+    return movieDate[0];
 }
 
 function generateHTMLfictionbooks(data, date) {
@@ -96,10 +135,33 @@ function generateHTMLfictionbooks(data, date) {
     console.log(fictionBookDate)
 }
 
+function generateHTMLnofictionbooks(data, date) {
+    
+    const birth = date // Serà la data que haurem introduit a la página index.html
+    let noFictionBookDate = data.filter((nofictionbook) => {
+        if (birth >= nofictionbook.startDate && birth <= nofictionbook.endDate) {
+            return true;
+        }
+        return false;
+    })
+    document.getElementById('no-fiction-book-name').innerHTML = noFictionBookDate[0].title
+    document.getElementById('writter-name-no-fiction').innerHTML = noFictionBookDate[0].author
+
+    console.log(noFictionBookDate)
+}
+
 function generateHTMLgenius(selectedElement){
-    document.getElementById('prueva').innerHTML = selectedElement.response.song.title
+    document.getElementById('song-name-genius').innerHTML = selectedElement.response.song.title
+    document.getElementById('artist-name-genius').innerHTML = selectedElement.response.song.primary_artist.name
+    document.getElementById('cd-name').innerHTML = selectedElement.response.song.album.name
+    document.getElementById('release-date').innerHTML = selectedElement.response.song.release_date
+    document.getElementById('song-tag').innerHTML = selectedElement.response.song.description.dom.children[0].children[0] + selectedElement.response.song.description.dom.children[0].children[0].children
     const albumImage = `<img src='${selectedElement.response.song.song_art_image_url}'>`
-   document.getElementById('album-img').innerHTML = albumImage;
+    document.getElementById('album-img').innerHTML = albumImage;
+    const artistImage = `<img src='${selectedElement.response.song.primary_artist.image_url}'>`
+    document.getElementById('artist-image').innerHTML = artistImage;
+    const cdImage = `<img src='${selectedElement.response.song.album.cover_art_url}'>`
+    document.getElementById('cd-image').innerHTML = cdImage;
 }
 
 
@@ -116,9 +178,13 @@ window.addEventListener('load', async (event) => {
     const responsejson = await getGeniusElements(currentSong.songTitle, currentSong.artist);
     generateHTMLgenius(responsejson);
     const allmovies = await getMovies();
-    generateHTMLmovies(allmovies, date);
+    const selectedMovie = generateHTMLmovies(allmovies, date);
+    const responsejsonmovies = await getIMDBElements(selectedMovie.movieTitle)
+    console.log(responsejsonmovies);
     const allfictionbooks = await getFictionBooks();
     generateHTMLfictionbooks(allfictionbooks, date);
+    const allnofictionbooks = await getnoFictionBooks();
+    generateHTMLnofictionbooks(allnofictionbooks, date);
 });
 
 
